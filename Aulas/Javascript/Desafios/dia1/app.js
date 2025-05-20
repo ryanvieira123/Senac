470104d372e50f4252baed903da2b0217075d7b9
ryanviera05 <ryan.assumpcao@outlook.com>
2025-05-19 22:14:35 -0300

    19.5/ briefing projeto integrador - desafios JS

---
+++ Aulas/Javascript/Desafios/dia1/app.js
@@ -0,0 +1,8 @@
+//Dia 1
+//Missão: Pedir o nome e a idade do usuário e escrever essas
+//informações de uma forma amigável.
+//Objetivo:
+//● Conhecer o console
+//● Trabalhar comandos de entrada e saída de dados do JavaScript
+
+
---
+++ Aulas/Javascript/Desafios/dia3/app.js
@@ -0,0 +1,5 @@
+let jan = 2, fev = 3, mar = 5
+
+let media = jan + fev + mar
+
+console.log(media)

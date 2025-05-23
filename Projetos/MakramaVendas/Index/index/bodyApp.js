997c49596a801ddb87b1731daebcb628b823740d
ryanviera05 <ryan.assumpcao@outlook.com>
2025-05-22 22:16:05 -0300

    Index refinado, addeventlistener

---
+++ Projetos/MakramaVendas/Index/index/bodyApp.js
@@ -0,0 +1,7 @@
+let amostra1 = document.getElementById('amostra1'); //selecionando o elemento(div) amostra1 
+                                                         //variável nomeada amostra1
+
+amostra1.addEventListener('click', function redirect(){  //esperando um evento click ocorrer na minha variável amostra1, qual tal está armazenando, no momento, um esperado clique na sua selecionada div, após o clique for identificado, uma função é chamada, 
+window.location.href = 'Ryan Vieira Senac 2025\Repo\Senac\Senac\Projetos\MakramaVendas\produtos\produtoIndex\produto.html';
+})
+

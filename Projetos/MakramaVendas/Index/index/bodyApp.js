5294fdfe950e8252fe20bf6c1d83a7112c5cf9f7
ryanviera05 <ryan.assumpcao@outlook.com>
2025-05-28 22:18:52 -0300

    product page basic template

--- Projetos/MakramaVendas/Index/index/bodyApp.js
+++ Projetos/MakramaVendas/Index/index/bodyApp.js
@@ -1,7 +1,10 @@
-let amostra1 = document.getElementById('amostra1'); //selecionando o elemento(div) amostra1 
-                                                         //variável nomeada amostra1
 
-amostra1.addEventListener('click', function redirect(){  //esperando um evento click ocorrer na minha variável amostra1, qual tal está armazenando, no momento, um esperado clique na sua selecionada div, após o clique for identificado, uma função é chamada, 
-window.location.href = 'Ryan Vieira Senac 2025\Repo\Senac\Senac\Projetos\MakramaVendas\produtos\produtoIndex\produto.html';
+
+
+//onjetivo, fazer com que uma div; seja clicável, e que direcione o usuário para outra página 
+
+const teste1 = document.getElementById("amostra1")
+
+teste1.addEventListener('click',  function redirect(){
+window.location.href = 'C:\Ryan Vieira Senac 2025\Repo\Senac\Senac\Projetos\MakramaVendas\produtos\index.html'
 })
-
---
+++ Projetos/MakramaVendas/produtos/app.js
@@ -0,0 +1,19 @@
+let produto1 = document.getElementById("produto1")
+let produto2 = document.getElementById("produto2")
+let produto3 = document.getElementById("produto3")
+let produto4 = document.getElementById("produto4")
+
+
+
+produto1.addEventListener('click', function redirect(){
+    window.location.href = 'Ryan Vieira Senac 2025\Repo\Senac\Senac\Projetos\MakramaVendas\produtos\produto1\produto1.html'})
+
+produto2.addEventListener('click', function redirect(){
+    window.location.href = 'Ryan Vieira Senac 2025\Repo\Senac\Senac\Projetos\MakramaVendas\produtos\produto1\produto2.html'})
+
+produto3.addEventListener('click', function redirect(){
+        window.location.href = 'Ryan Vieira Senac 2025\Repo\Senac\Senac\Projetos\MakramaVendas\produtos\produto1\produto3.html'})
+
+produto4.addEventListener('click', function redirect(){
+            window.location.href = 'Ryan Vieira Senac 2025\Repo\Senac\Senac\Projetos\MakramaVendas\produtos\produto1\produto4.html'})
+

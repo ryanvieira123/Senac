9454e276fb0b9f1ec0f12be65ea2293250e16d33
ryanviera05 <ryan.assumpcao@outlook.com>
2025-06-02 22:15:11 -0300

    Nutrition home page

--- Projetos/MakramaVendas/Index/index/bodyApp.js
+++ Projetos/MakramaVendas/Index/index/bodyApp.js
@@ -1,10 +1,2 @@
 
 
-
-//onjetivo, fazer com que uma div; seja clicável, e que direcione o usuário para outra página 
-
-let produtos = document.getElementById("produtos")
-
-teste1.addEventListener('click',  function redirect(){
-window.location.href = 'Ryan Vieira Senac 2025\Repo\Senac\Senac\Projetos\MakramaVendas\produtos\produtos.html'
-})
---
+++ Projetos/Nutrição/Home/header.js
@@ -0,0 +1,5 @@
+let receitas = document.getElementById('receitas')
+
+receitas.addEventListener('click', function(){
+    location.href = 'Projetos\Nutrição\Receitas\receitas.html'
+})

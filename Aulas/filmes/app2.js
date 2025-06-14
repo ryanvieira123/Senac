7af7aa095777ef8f548f98a027a8d039b61857a6
ryanviera05 <90793947+ryanvieira123@users.noreply.github.com>
2025-06-14 20:04:53 -0300

    14.6 version(casa)

--- Aulas/filmes/app2.js
+++ Aulas/filmes/app2.js
@@ -1,7 +1,17 @@
 async function fnPegarFilmes() {  //async faz com que o restante do código seja executado enquanto a função aguarda o seu término                 
-    let filmes = await fetch("dados-filmes.json") //método fetch acompanhado de uma função await, espera a resposta do fetch antes de seguir
+    let filmes = await fetch("dados-filmes.json") //método fetch acompanhado de uma função await, espera a resposta do fetch antes de seguir pra próxima linha dentro da função
     console.dir(filmes)     //console.dir, exibe arquivos 
+    
     let filmesTratados = await filmes.json() 
+
+    filmesTratados.slice(14,17).forEach(function (){
+     filmesTratados.querySelector(FilmeAtual).innerHTML +=
+     `
+     <div>$(filmeAtual.sinopse)</div>
+     `
+    })
+
+
     console.dir(filmesTratados)
 }
 
@@ -10,4 +20,4 @@ async function fnPegarFilmes() {  //async faz com que o restante do código seja
 fnPegarFilmes()
 console.log("oi")
 
-  
+  // dando fetch num arquivo json, contendo uma array contendo algumas variáveis, e trazendo-as para o index, numa div pré-existente
